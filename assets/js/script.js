14ff5ca0f4df098c86153263358a73fb361b47d1
MCassa99 <m_cassa@icloud.com>
2023-09-02 14:05:51 -0300

    Mejoras

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-document.getElementById("noticias").onload = function() {appendli()};
+// Este archivo se encarga de cargar los mensajes del archivo JSON
 
-function appendli() {
-    const comment = 2;
-    while (comment > 0) {
-        var li = document.getElementById("noticias");
-        li.innerHTML += `<li class='mt-2'>Nuevo Comentario</li><hr>`; 
-        comment = comment - 1;
+// Importamos el archivo JSON
+import messages from "../json/avisos.json";
+
+// Creamos una variable para almacenar los mensajes
+const messagesArray = messages;
+
+// Creamos una función para mostrar los mensajes
+function showMessages() {
+    // Recorremos el array de mensajes
+    for (const message of messagesArray) {
+        // Mostramos cada mensaje
+        document.getElementById("noticias").innerHTML += `<li class='mt-2'>${message.message}</li><hr>`;
     }
 }
+alert('AAAAAA')
+// Llamamos a la función para mostrar los mensajes
+showMessages();
+
